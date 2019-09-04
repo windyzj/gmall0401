39682779b5b8f0005d804bd63df19f64e0fe6eff
zhangchen <windyzj@163.com>
2019-09-04 10:29:06 +0800

    加入用户管理模块

---
+++ gmall0401-user-manage/src/main/java/com/atguigu/gmall0401/user/Gmall0401UserManageApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall0401.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0401UserManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0401UserManageApplication.class, args);
+    }
+
+}
